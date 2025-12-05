e22a959901c99e5d230a97acd3b2dd1f1c393ff8
belufelu <belufelu1@gmail.com>
2025-12-05 10:48:44 +0300

    Create notify.lua

---
+++ notify.lua
@@ -0,0 +1,8 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
+Notification:Notify(
+    {Title = "TEXT TITLE", Description = "DESCRIPTION"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
